127cef043fccd18cba20fb9a10cdb53d482c32ed
benhuynh003 <155077782+benhuynh003@users.noreply.github.com>
2024-06-30 22:17:19 -0700

    test

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -15,3 +15,4 @@ const ServiceCard = ({ imgURL, label, subtext }) => {
 };
 
 export default ServiceCard;
+
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,11 +1,11 @@
-import { copyrightSign } from "../assets/icons";
-import { footerLogo } from "../assets/images";
-import { footerLinks, socialMedia } from "../constants";
+import { copyrightSign } from '../assets/icons'
+import { socialMedia, footerLinks } from '../constants'
+import { footerLogo } from '../assets/images'
 
 const Footer = () => {
   return (
-    <footer className='max-container'>
-      <div className='flex justify-between items-start gap-20 flex-wrap max-lg:flex-col'>
+    <footer className="max-container">
+       <div className='flex justify-between items-start gap-20 flex-wrap max-lg:flex-col'>
         <div className='flex flex-col items-start'>
           <a href='/'>
             <img
@@ -53,21 +53,15 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className='flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center'>
-        <div className='flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer'>
-          <img
-            src={copyrightSign}
-            alt='copyright sign'
-            width={20}
-            height={20}
-            className='rounded-full m-0'
-          />
-          <p>Copyright. All rights reserved.</p>
-        </div>
-        <p className='font-montserrat cursor-pointer'>Terms & Conditions</p>
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+          <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+            <img src={copyrightSign} alt="copy right sign" width={20} height={20} className="rounded-full m-0"/>
+            <p>Copyright. All rights reserved.</p>
+            <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
+          </div>
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
